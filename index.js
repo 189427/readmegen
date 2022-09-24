321eed80302310e32625712b6fb90cc4142c7bac
Brian Weinthal <76851117+189427@users.noreply.github.com>
2022-09-24 17:29:03 -0400

    added updated code

--- index.js
+++ index.js
@@ -1,13 +1,80 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "Project name?",
+  },
+  {
+    type: "input",
+    name: "Description",
+    message: "Description of the project?",
+  },
+  {
+    type: "input",
+    name: "Installation Instructions",
+    message: "Please add installation instructions",
+  },
+  {
+    type: "input",
+    name: "Usage",
+    message: "Please add usage information",
+  },
+  {
+    type: "input",
+    name: "Contribution",
+    message: "Please add contribution information",
+  },
+  {
+    type: "input",
+    name: "Testing",
+    message: "Please add testing instructions",
+  },
+  {
+    type: "list",
+    name: "License",
+    message: "please choose a license",
+    choices: ["GNU", "MIT", "Unilicense", "No license"],
+    filter(val) {
+      return val.toUpperCase();
+    },
+  },
+  {
+    type: "input",
+    name: "github username",
+    message: "Please enter your github username",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please enter your email address",
+  },
+];
 
-// TODO: Create a function to write README file
+//run question function
+async function runQ() {
+  return inquirer
+    .prompt(questions)
+    .then((answers) => {
+      console.log(answers);
+      return answers;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+runQ();
+
+/* // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
-init();
+init(); */
